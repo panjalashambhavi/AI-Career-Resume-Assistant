06674c86a9724ce329b2840103bb845ed557b5fd
sham <pshambhavi701@gmail.com>
2026-07-23 15:01:37 +0530

    Fix indentation

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -10,7 +10,9 @@ client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
+
 def analyze_resume(resume_text):
+
     prompt = f"""
 You are an expert ATS Resume Analyzer.
 
@@ -34,8 +36,9 @@ Resume:
 """
 
     try:
+
         response = client.models.generate_content(
-            model="gemini-2.5-flash-lite",
+            model="gemini-2.5-flash",
             contents=prompt
         )
 
@@ -47,25 +50,28 @@ Resume:
         return json.loads(text)
 
     except Exception as e:
-    error = str(e)
 
-    if "RESOURCE_EXHAUSTED" in error:
+        traceback.print_exc()
+
+        error = str(e)
+
+        if "RESOURCE_EXHAUSTED" in error:
+            return {
+                "summary": "Gemini free quota exceeded. Please try again after one minute.",
+                "ats_score": 0,
+                "strengths": [],
+                "weaknesses": ["Gemini quota exceeded"],
+                "missing_skills": [],
+                "suggestions": [],
+                "interview_questions": []
+            }
+
         return {
-            "summary": "AI service is temporarily busy. Please try again after one minute.",
+            "summary": error,
             "ats_score": 0,
             "strengths": [],
-            "weaknesses": ["Gemini free quota exceeded."],
+            "weaknesses": [error],
             "missing_skills": [],
             "suggestions": [],
             "interview_questions": []
         }
-
-    return {
-        "summary": error,
-        "ats_score": 0,
-        "strengths": [],
-        "weaknesses": [error],
-        "missing_skills": [],
-        "suggestions": [],
-        "interview_questions": []
-    }
